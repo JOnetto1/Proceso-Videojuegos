0d472442203259e88bd018105c53dfc5eb6c5d7a
JOnetto1 <josefina.onetto@sansano.usm.cl>
2023-06-19 17:39:02 -0400

    PlayerMovement
    Adding player object and movement. TODO: player sprite with walking animations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 direction;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TakeInput();
+        Move();
+    }
+
+    private void TakeInput()
+    {
+        direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.UpArrow)){
+
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += Vector2.right;
+
+        }
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        if(direction.x != 0 || direction.y != 0)
+        {
+            SetAnimatorMovement(direction);
+        }
+    }
+
+    private void SetAnimatorMovement(Vector2 direction)
+    {
+        animator.SetFloat("xDir", direction.x);
+        animator.SetFloat("yDir", direction.y);
+    }
+}
